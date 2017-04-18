ee4e33c00c7b806a663508aa5c609f1a644087d2
arobillard <robi0653@algonquinlive.com>
2017-04-18 16:53:13 -0400

    Markbot Fixes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var newName = prompt('What is your name?');
 
 if (newName == 'Thomas') {
-  alert('Hey' + ' ' + newName + ' ' + 'our names are the same!');
+  alert('Names are the same!');
 } else {
-  alert('Too bad our names are different, ' + newName + '.');
+  alert('Names are different.');
 }
